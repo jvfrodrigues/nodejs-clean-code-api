9a802482967a0c8126ad2b0f11ccc00b107aefb8
Joao Rodrigues <jvfr96@gmail.com>
2021-05-08 17:53:03 -0300

    feat: ensure SignUpController returns 400 if no name is provided

---
+++ src/presentation/controllers/signup.ts
@@ -0,0 +1,10 @@
+// Vai colocando o minimo possivel para poder fazer rodar os testes, quantos mais teste forem feitos, mais será visivel a necessidade de adicionar novas condiçoes, verificaçoes, refatorando o codigo ate o funcionamento total
+// Deve primeiro comitar a classe de produção antes dos testes, para nao dar errado por arquivo nao existir
+
+export class SignUpController {
+  handle(httpRequest: any): any {
+    return {
+      statusCode: 400
+    }
+  }
+}
